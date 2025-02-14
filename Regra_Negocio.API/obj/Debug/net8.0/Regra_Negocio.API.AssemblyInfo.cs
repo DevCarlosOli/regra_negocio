9ca86a4fe7157c3e92d8c577848109117aaa980e
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-02-14 00:08:10 -0300

    ♻️ refactor: Caminho do projeto de testes

--- Regra_Negocio.API/obj/Debug/net8.0/Regra_Negocio.API.AssemblyInfo.cs
+++ Regra_Negocio.API/obj/Debug/net8.0/Regra_Negocio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Regra_Negocio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a864ec89f5940c1e04cd8677a41a63cc39c28a42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Regra_Negocio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Regra_Negocio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
